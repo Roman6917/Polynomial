a8c2e51ce56e91747a2c9dde29e1a11933bf7f6d
Roman <kordukroman@gmail.com>
2018-03-31 21:58:15 +0300

    Some Fixes

--- Polynomial.Test/UnitTest1.cs
+++ Polynomial.Test/UnitTest1.cs
@@ -7,7 +7,8 @@ namespace Polynomial.Test
 {
 	public class UnitTest1
 	{
-		private static Monomial monomial { get; } = new Monomial(1,5);
+		private static Monomial monomial { get; } = new Monomial(1, 5);
+
 		private static Polynomial polynomial { get; } = new Polynomial(monomial,
 			monomial,
 			monomial.Derivative(),
@@ -21,36 +22,34 @@ namespace Polynomial.Test
 		{
 			Assert.Equal(expected, source);
 		}
-		
+
 		[Theory]
 		[MemberData(nameof(MonomialData.DataDerivative), MemberType = typeof(MonomialData))]
 		public void MonomialDerivativeObjectTest(string source, string expected)
 		{
 			Assert.Equal(expected, source);
 		}
-		
+
 		[Theory]
 		[MemberData(nameof(MonomialData.ThrowsData), MemberType = typeof(MonomialData))]
 		public void TestAddMonomialThrows(Monomial mon)
 		{
 			Assert.Throws<ArgumentException>(() => monomial.Add(mon));
 		}
-		
+
 		[Theory]
 		[MemberData(nameof(PolynomialData.Data), MemberType = typeof(PolynomialData))]
 		public void PolynomialObjectTest(string source, string expected)
 		{
 			Assert.Equal(expected, source);
 		}
-		
+
 		[Theory]
 		[MemberData(nameof(PolynomialData.DataDerivative), MemberType = typeof(PolynomialData))]
 		public void PolynomialDerivativeObjectTest(string source, string expected)
 		{
 			Assert.Equal(expected, source);
 		}
-
-		
 
 
 		public class MonomialData
@@ -71,7 +70,7 @@ namespace Polynomial.Test
 				new object[] {new Monomial(24, 75).ToString(), "24x^75"},
 				new object[] {new Monomial(45, 7).ToString(), "45x^7"},
 			};
-			
+
 			public static IEnumerable<object[]> DataDerivative => new List<object[]>
 			{
 				new object[] {new Monomial(1, 2).Derivative(1).ToString(), "2x"},
@@ -81,10 +80,9 @@ namespace Polynomial.Test
 				new object[] {new Monomial(14, 5).Derivative(2).ToString(), "280x^3"},
 			};
 		};
-		
+
 		public class PolynomialData
 		{
-
 			public static IEnumerable<object[]> Data => new List<object[]>
 			{
 				new object[] {polynomial.ToString(), "x^5 + x^5 + 5x^4 + 20x^3 + 60x^2 + 120x"},
@@ -103,14 +101,13 @@ namespace Polynomial.Test
 						new Monomial(4, 8).Derivative(3),
 						new Monomial(1, 5).Derivative(1),
 						new Monomial(27, 4),
-						new Monomial(46,4).Derivative(),
-						new Monomial(257,7).Derivative(5), 
-						new Monomial(160,1).Derivative()).ToString(), 
+						new Monomial(46, 4).Derivative(),
+						new Monomial(257, 7).Derivative(5),
+						new Monomial(160, 1).Derivative()).ToString(),
 					"130478439168000x^15 + 1344x^5 + 5x^4 + 27x^4 + 184x^3 + 647640x^2 + 160"
 				},
-
 			};
-			
+
 			public static IEnumerable<object[]> DataDerivative => new List<object[]>
 			{
 				new object[] {polynomial.Derivative(1).ToString(), "5x^4 + 5x^4 + 20x^3 + 60x^2 + 120x + 120"},
--- Polynomial/Program.cs
+++ Polynomial/Program.cs
@@ -2,193 +2,167 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-// ReSharper disable NonReadonlyMemberInGetHashCode
-// ReSharper disable AccessToModifiedClosure
-// ReSharper disable CompareOfFloatsByEqualityOperator
+
 
 namespace Polynomial
 {
-	public class Monomial: ICloneable
+	public class Monomial : ICloneable
+	{
+		public Monomial(double coefficient = 0, int power = 0)
 		{
-			public Monomial(double coefficient = 0, int power = 0)
+			Power = power;
+			Coefficient = coefficient;
+		}
+
+		private int Power { get; set; }
+		private double Coefficient { get; set; }
+		public static readonly Monomial Zero = new Monomial();
+
+		public Monomial Derivative(uint iterations = 1)
+		{
+			Monomial result;
+			if (Power == 0 || Coefficient == 0)
 			{
-				Power = power;
-				Coefficient = coefficient;
+				result = Zero;
+			}
+			else if (iterations == 0)
+			{
+				result = this;
+			}
+			else
+			{
+				result = new Monomial(Coefficient * Power, Power - 1);
 			}
 
-			private int Power { get; set; }
-			private double Coefficient { get; set; }
-			public static readonly Monomial Zero = new Monomial();
+			if (iterations > 1)
+				result = result.Derivative(iterations - 1);
 
-			public Monomial Derivative(uint iterations = 1)
+			return result;
+		}
+
+		public override string ToString()
+		{
+			var coeff = "";
+			if (Coefficient != 1.0)
+				coeff = Coefficient + "";
+			var pow = "";
+			if (Power != 0.0 && Coefficient != 0)
 			{
-				Monomial result;
-				if (Power == 0 || Coefficient == 0)
-				{
-					result = Zero;
-				}
-				else if (iterations == 0)
-				{
-					result = this;
-				}
-				else
-				{
-					result = new Monomial(Coefficient * Power, Power - 1);
-				}
-
-				if (iterations > 1)
-					result = result.Derivative(iterations - 1);
-
-				return result;
+				pow = "x";
+				if (Power != 1.0)
+					pow += $"^{Power}";
 			}
 
-			public override string ToString()
-			{
-				var coeff = "";
-				if (Coefficient != 1.0)
-					coeff = Coefficient + "";
-				var pow = "";
-				if (Power != 0.0 && Coefficient != 0)
-				{
-					pow = "x";
-					if (Power != 1.0)
-						pow += $"^{Power}";
-				}
+			var result = $"{coeff}{pow}";
+			if (result == "")
+				result = $"{Coefficient}";
+			return result;
+		}
 
-				var result = $"{coeff}{pow}";
-				if (result == "")
-					result = $"{Coefficient}";
-				return result;
+		public object Clone()
+		{
+			return new Monomial(Coefficient, Power);
+		}
+
+		private bool Equals(Monomial other)
+		{
+			return Power.Equals(other.Power) && Coefficient.Equals(other.Coefficient);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			return obj.GetType() == GetType() && Equals((Monomial) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (Power * 397) ^ Coefficient.GetHashCode();
 			}
+		}
 
-			public object Clone()
-			{
-				return new Monomial(Coefficient, Power);
-			}
+		public static bool operator ==(Monomial left, Monomial right)
+		{
+			return Equals(left, right);
+		}
 
-			private bool Equals(Monomial other)
-			{
-				return Power.Equals(other.Power) && Coefficient.Equals(other.Coefficient);
-			}
+		public static bool operator !=(Monomial left, Monomial right)
+		{
+			return !Equals(left, right);
+		}
 
-			public override bool Equals(object obj)
-			{
-				if (ReferenceEquals(null, obj)) return false;
-				if (ReferenceEquals(this, obj)) return true;
-				return obj.GetType() == GetType() && Equals((Monomial) obj);
-			}
-
-			public override int GetHashCode()
-			{
-				unchecked
-				{
-					return (Power * 397) ^ Coefficient.GetHashCode();
-				}
-			}
-
-			public static bool operator ==(Monomial left, Monomial right)
-			{
-				return Equals(left, right);
-			}
-
-			public static bool operator !=(Monomial left, Monomial right)
-			{
-				return !Equals(left, right);
-			}
-
-			public Monomial Add(Monomial mon)
-			{
-				var newMon = (Monomial) Clone();
-				if (mon.Coefficient == 0) 
-					return newMon;
-				if (newMon.Coefficient == 0)
-					newMon.Power = mon.Power;
-				if (mon.Power != newMon.Power)
-					throw new ArgumentException("Power has to be the same");
-				newMon.Coefficient += mon.Coefficient;
+		public Monomial Add(Monomial mon)
+		{
+			var newMon = (Monomial) Clone();
+			if (mon.Coefficient == 0)
 				return newMon;
-			}
+			if (newMon.Coefficient == 0)
+				newMon.Power = mon.Power;
+			if (mon.Power != newMon.Power)
+				throw new ArgumentException("Power has to be the same");
+			newMon.Coefficient += mon.Coefficient;
+			return newMon;
 		}
+	}
 
-		public class Polynomial : IEnumerable
+	public class Polynomial : IEnumerable
+	{
+		private readonly List<Monomial> _monomials;
+
+		public Polynomial(params Monomial[] monomials)
 		{
-			private readonly List<Monomial> _monomials;
-
-			public Polynomial(params Monomial[] monomials)
-			{
-				_monomials = monomials.ToList();
-			}
-
-			public IEnumerator GetEnumerator()
-			{
-				return _monomials.GetEnumerator();
-			}
-
-			public override string ToString()
-			{
-				var result = "";
-				var counter = 0;
-				_monomials.ForEach(mon =>
-				{
-					counter++;
-					result += mon.ToString();
-					if (counter != _monomials.Count)
-						result += " + ";
-				});
-				if (result == "")
-					result = "0";
-				return result;
-			}
-
-			private void Add(Monomial mon)
-			{
-				_monomials.Add(mon);
-			}
-/*
-
-			private void Normalize()
-			{
-				var items = new List<Monomial>();
-				var maxPow = int.MinValue;
-				_monomials.ForEach(mon =>
-				{
-					if (mon.Power > maxPow)
-					{
-						maxPow = mon.Power;
-						items.AddRange(Enumerable.Repeat(Monomial.Zero, maxPow - items.Count));
-					}
-
-					Console.Out.WriteLine("mon = {0}", mon);
-					Console.Out.WriteLine("mon.Power - 1 = {0}", mon.Power - 1);
-					Console.Out.WriteLine("items[mon.Power - 1] = {0}", items[mon.Power - 1]);
-					items[mon.Power - 1] = items[mon.Power - 1].Add(mon);
-				});
-				_monomials = items;
-			}
-*/
-
-			public Polynomial Derivative(uint iterations = 1)
-			{
-				var poli = new Polynomial();
-
-				if (iterations == 0)
-					return this;
-
-				foreach (var mon in _monomials)
-				{
-					var derivative = mon.Derivative(iterations);
-					if (Equals(derivative, Monomial.Zero)) continue;
-					poli.Add(derivative);
-				}
-
-//				if (iterations > 1)
-//					poli = poli.Derivative(iterations - 1);
-				return poli;
-			}
+			_monomials = monomials.ToList();
 		}
+
+		public IEnumerator GetEnumerator()
+		{
+			return _monomials.GetEnumerator();
+		}
+
+		public override string ToString()
+		{
+			var result = "";
+			var counter = 0;
+			_monomials.ForEach(mon =>
+			{
+				counter++;
+				result += mon.ToString();
+				if (counter != _monomials.Count)
+					result += " + ";
+			});
+			if (result == "")
+				result = "0";
+			return result;
+		}
+
+		private void Add(Monomial mon)
+		{
+			_monomials.Add(mon);
+		}
+
+		public Polynomial Derivative(uint iterations = 1)
+		{
+			var poli = new Polynomial();
+
+			if (iterations == 0)
+				return this;
+
+			foreach (var mon in _monomials)
+			{
+				var derivative = mon.Derivative(iterations);
+				if (Equals(derivative, Monomial.Zero)) continue;
+				poli.Add(derivative);
+			}
+
+			return poli;
+		}
+	}
 
 	public static class Program
-	{		
+	{
 		public static void Main()
 		{
 			var mon = new Monomial(1, 5);
